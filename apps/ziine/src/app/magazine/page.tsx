3478ff0029d74767bcd34fd0e014c7e54f5b4e44
narinn-star <skfls2618@naver.com>
2025-02-08 03:13:20 +0900

    Feat : register_artwork Page

---
+++ apps/ziine/src/app/magazine/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MagazinePage = () => {
+  return <h1>MagazinePage</h1>;
+};
+
+export default MagazinePage;
---
+++ apps/ziine/src/app/register_artwork/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RegisterArtworkPage = () => {
+  return <h1 className="centerContainer">RegisterArtworkPage</h1>;
+};
+
+export default RegisterArtworkPage;
